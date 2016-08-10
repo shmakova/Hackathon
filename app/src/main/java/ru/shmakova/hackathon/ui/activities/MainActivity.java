11781e0ad6e938b1df02cf8719eee657451c49ba
shmakova <shmakova-nastya@yandex.ru>
2016-08-10 17:25:11 +0300

    getSupportFragmentManager to supportFragmentManager

--- app/src/main/java/ru/shmakova/hackathon/ui/activities/MainActivity.java
+++ app/src/main/java/ru/shmakova/hackathon/ui/activities/MainActivity.java
@@ -3,6 +3,7 @@ package ru.shmakova.hackathon.ui.activities;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
 
 import ru.shmakova.hackathon.App;
 import ru.shmakova.hackathon.R;
@@ -10,16 +11,19 @@ import ru.shmakova.hackathon.ui.fragments.CardFragment;
 import ru.shmakova.hackathon.ui.fragments.MainFragment;
 import ru.shmakova.hackathon.ui.fragments.PronunciationFragment;
 
-public class MainActivity extends BaseActivity implements MainFragment.OnMenuItemClickListener  {
+public class MainActivity extends BaseActivity implements MainFragment.OnMenuItemClickListener {
+    private FragmentManager supportFragmentManager;
+
     @SuppressLint("InflateParams") // It's okay in our case.
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         App.get(this).applicationComponent().inject(this);
         setContentView(R.layout.activity_main);
+        supportFragmentManager = getSupportFragmentManager();
 
         if (savedInstanceState == null) {
-            getSupportFragmentManager()
+            supportFragmentManager
                     .beginTransaction()
                     .replace(R.id.main_frame_layout, new MainFragment())
                     .commit();
@@ -28,19 +32,21 @@ public class MainActivity extends BaseActivity implements MainFragment.OnMenuIte
 
     /**
      * Opens fragment
+     *
      * @param item text of menu item
      */
     @Override
     public void onMenuItemClick(String item) {
         if (item.equals(getString(R.string.cards))) {
-            getSupportFragmentManager()
+            supportFragmentManager
                     .beginTransaction()
                     .replace(R.id.main_frame_layout, new CardFragment())
                     .addToBackStack(null)
                     .commit();
+            return;
         }
         if (item.equals(getString(R.string.words_pronunciation))) {
-            getSupportFragmentManager()
+            supportFragmentManager
                     .beginTransaction()
                     .replace(R.id.main_frame_layout, new PronunciationFragment())
                     .addToBackStack(null)
